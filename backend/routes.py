145ff82cf7f64c830d0d58848db566448aa180a0
Ben Avtabi <ben.avtabi@gmail.com>
2022-08-06 14:43:08 +0300

    Allow cross origin

--- backend/routes.py
+++ backend/routes.py
@@ -6,11 +6,13 @@ from mobs_responder import MobsResponder
 app = Flask(__name__)
 
 @app.route('/recommended_maps/<level>')
+@cross_origin()
 def recommended_maps(level):
 	responder = RecommendedMapsResponder(level)
 	return responder.get_response()
 
 @app.route('/mobs/<map_id>')
+@cross_origin()
 def mobs(map_id):
 	responder = MobsResponder(map_id)
 	return responder.get_response()
